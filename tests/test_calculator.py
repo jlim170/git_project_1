62f925bfc024b0fb4e6dffe5aafaa73d4950c013
Matthew Nguyen <mnguye31@terpmail.umd.edu>
2026-03-27 10:33:43 -0400

    Add additional factorial test case

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -27,6 +27,8 @@ def test_factorial():
     assert factorial(0) == 1
     assert factorial(5) == 120
     assert factorial(3) == 6
+    assert factorial(1) == 1
+    assert factorial(6) == 720
 
 def test_factorial_negative():
     with pytest.raises(ValueError):
